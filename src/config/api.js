e29d3ab71cd040ce6642cc36bc628087a9621187
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2024-07-24 02:55:58 +0530

    Modified api url to appwrite api endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,11 @@
 export const CoinList = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+  `https://66a002cb7ad90f048a98.appwrite.global/coinList/${currency}`;
 
 export const SingleCoin = (id) =>
-  `https://api.coingecko.com/api/v3/coins/${id}`;
+  `https://66a002cb7ad90f048a98.appwrite.global/singleCoin/${id}`;
 
 export const HistoricalChart = (id, days = 365, currency) =>
-  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+  `https://66a002cb7ad90f048a98.appwrite.global/historicalChart/${id}/${currency}/${days}`;
 
 export const TrendingCoins = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+  `https://66a002cb7ad90f048a98.appwrite.global/trendingCoins/${currency}`;
